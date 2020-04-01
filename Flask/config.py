359e7f8a91c2d97cb6156c88a454bf0e459e186b
Tung <tungbtruong@gmail.com>
2020-03-31 19:49:21 -0600

    Added extra files such as views.py and refactored some code around. Please read reorganization.txt and do not merge because I just broke a lot of connections, so the app is not running for now

--- Flask/config.py
+++ Flask/config.py
@@ -1,7 +1,12 @@
+
+IMPORT STATEMENTS
+
+
+
+
 UPLOAD_FOLDER = 'uploads'
 UPLOAD_JSON = 'json'
 UPLOAD_FINAL = 'static'
-app = Flask(__name__) 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER #configuration on startup
 app.config['UPLOAD_JSON'] = UPLOAD_JSON
