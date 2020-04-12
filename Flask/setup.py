94196c5157d6d5e79995202e532f8a0682dd0f01
Gaby Nitorreda <gabriela.nitorreda@gmail.com>
2020-04-12 19:11:23 -0400

    Added setup.py; got test helloworld route running to confirm working
    Coauthored by: Tung <49893620+tbtruong@users.noreply.github.com>

---
+++ Flask/setup.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, url_for, flash, render_template 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from setuptools import setup
+
+from SCDA import app
+
+"""
+setup(
+    name='SCDA',
+    packages=['SCDA'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+class temporaryModel(db.Model):
+    __tablename__ = 'TEST'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String())
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"<Constituent {self.name}>"
+"""
