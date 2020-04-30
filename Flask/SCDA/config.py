b1014c31476a338c3b6fd7f51576432d2c6a4034
Gaby Nitorreda <gabriela.nitorreda@gmail.com>
2020-04-29 21:54:13 -0400

    delocalized upload locations again

--- Flask/SCDA/config.py
+++ Flask/SCDA/config.py
@@ -1,14 +1,14 @@
 import os
-basedir = os.path.abspath('uploads')
-#print(basedir)
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
 #from SCDA import app
 
 
-UPLOAD_FOLDER = os.path.abspath('uploads')
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname('uploads'))
 UPLOAD_JSON = 'json'
-UPLOAD_FINAL = os.path.abspath('static')
-app.config['DEBUG'] = True
+UPLOAD_FINAL = os.path.abspath(os.path.dirname('static'))
 
+app.config['DEBUG'] = True
 #https://docs.sqlalchemy.org/en/13/core/engines.html
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@localhost/constituents"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER #configuration on startup
