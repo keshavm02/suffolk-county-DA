0aae61591761e1c0b84a16174c96de8a66a908eb
Gaby Nitorreda <gabriela.nitorreda@gmail.com>
2020-04-29 22:08:45 -0400

    delocalized upload locations again

--- Flask/SCDA/config.py
+++ Flask/SCDA/config.py
@@ -4,9 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 from SCDA import app
 
 
-UPLOAD_FOLDER = os.path.abspath(os.path.dirname('uploads'))
+UPLOAD_FOLDER = os.path.abspath('uploads')
+#print(UPLOAD_FOLDER)
 UPLOAD_JSON = 'json'
-UPLOAD_FINAL = os.path.abspath(os.path.dirname('static'))
+UPLOAD_FINAL = os.path.abspath('static')
+#print(UPLOAD_FINAL)
 
 app.config['DEBUG'] = True
 #https://docs.sqlalchemy.org/en/13/core/engines.html
