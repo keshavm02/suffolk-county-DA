dfe6322acce52de713bac6d7c5d38fec55938bf2
Keshav <km02@bu.edu>
2020-05-01 00:33:01 -0400

    put the upload function in a try except block

--- Flask/SCDA/routes.py
+++ Flask/SCDA/routes.py
@@ -33,75 +33,78 @@ def admin():
     
 @app.route('/upload', methods=['POST', 'GET'])
 def upload_forms():
-    if request.method == 'POST':
-        print("POST REQUEST RECEIVED")
-        print(request.files)
-        form_data = request.files
-        form_upload_date = datetime.now()
-        uuid = -1
-        checkRequiredForms = checkAllRequiredForms(form_data)
-        if (not checkRequiredForms[0]):
-            return checkRequiredForms[1]
-        else:
-            acc_file = form_data['acc']
-            acc_filename = acc_file.filename
-            if acc_filename == "":
-                return redirect('failure')
-            if isFileAllowed(acc_file):
-                acc_filename = secure_filename(acc_filename)
-                acc_file = Image.open(acc_file)
-                doc, image_path = localUploadAndExtraction(acc_filename, acc_file)
-                #FOR TESTING
-                path = os.path.abspath("../SCDA/extract_text/extraction_tests/test_textdumps/Application for Criminal Complaint .txt")
-                doc = open(path).read()
-                acc_info = extract_application_for_criminal_complaint(doc)
-                if acc_info["Name"] == '' or acc_info["Date of Birth"] == '' or acc_info["Social Security No."] == '':
-                    return "Application for criminal complaint could not be read. Please upload a clearer image."
-                else:
-                    uuid = getUserID(acc_info["Name"], acc_info["Social Security No."][-4:], acc_info["Date of Birth"])
-                    formTable = models.forms(uuid, form_upload_date, form_upload_date, form_upload_date, form_upload_date)
-                    acc_insert = models.ACC(uuid, form_upload_date, image_path, acc_info["Summons"], acc_info["Hearing Requested"], acc_info["Court"], acc_info["Arrest Status of Accused"], acc_info["Arrest Date"], acc_info["In Custody"],acc_info["Officer ID No."],acc_info["Agency"],acc_info["Type"],acc_info["Name"],acc_info["Birth Surname"],acc_info["Address"],acc_info["Date of Birth"], acc_info["Place of Birth"], acc_info["Social Security No."], acc_info["PCF No."],acc_info["SID"],acc_info["Marital Status"],acc_info["Driver's License No."],acc_info["Driver's License State"],acc_info["Driver's License Exp. Year"],acc_info["Gender"],acc_info["Race"],acc_info["Height"],acc_info["Weight"],acc_info["Eyes"],acc_info["Hair"],acc_info["Ethnicity"],acc_info["Primary Language"],acc_info["Complexion"],acc_info["Scars/Marks/Tattoos"],acc_info["Employer Name"],acc_info["School Name"],acc_info["Day Phone"],acc_info["Mother Name"],acc_info["Mother Maiden Name"],acc_info["Father Name"],acc_info["Complainant Type"],acc_info["Police Dept."])
-                    db.session.add(formTable)
-                    db.session.commit()
-                    db.session.add(acc_insert)
-                    db.session.commit()
-            cc_file = form_data['cc']
-            cc_filename = cc_file.filename
-            if cc_filename == "":
-                return redirect('failure')
-            if isFileAllowed(cc_file):
-                cc_filename = secure_filename(cc_filename)
-                cc_file = Image.open(cc_file)
-                doc, image_path = localUploadAndExtraction(cc_filename, cc_file)
-                cc_info = extract_criminal_complaint(doc)
-                cc_insert = models.CC(uuid, form_upload_date, image_path, cc_info["docket"], cc_info["name"], cc_info["dob"], cc_info["doc"], 
-                         cc_info["doo"], cc_info["doa"], cc_info["ned"], cc_info["obtn"], cc_info["irn"], cc_info["court_address"], 
-                         cc_info["defendant_address"], cc_info["offense_codes"])
-                db.session.add(cc_insert)
-                db.session.commit()
-            ir_file = form_data['ir']
-            ir_filename = ir_file.filename
-            if ir_filename == "":
-                return redirect('failure')
-            if isFileAllowed(ir_file):
-                ir_filename = secure_filename(ir_filename)
-                ir_file = Image.open(ir_file)
-                doc, image_path = localUploadAndExtraction(ir_filename, ir_file)
-                ir_info = extract_incident_report(doc)
-                ir_insert = models.IR(uuid, form_upload_date, image_path, ir_info["Case Number"],ir_info["CAD Incident Number"],ir_info["Report Type"],ir_info["Date / Time Occurred"],ir_info["Date / Time Reported"],ir_info["Public Narrative"])
-                db.session.add(ir_insert)
-                db.session.commit()
+    try:
+        if request.method == 'POST':
+            print("POST REQUEST RECEIVED")
+            print(request.files)
+            form_data = request.files
+            form_upload_date = datetime.now()
+            uuid = -1
+            checkRequiredForms = checkAllRequiredForms(form_data)
+            if (not checkRequiredForms[0]):
+                return checkRequiredForms[1]
             else:
-                return redirect('failure')
-            addOptional = addOptionalForms(form_data, uuid, formTable, form_upload_date)
-            print(addOptional)
-            if not addOptional:
-                return redirect('failure')
-            return redirect('success')
-    elif request.method == 'GET':
-        print('GET request received.')
-        return render_template('admin.html')
-        #return 'Please send a post request with at least the following forms: Application for Criminal Complaint, Criminal Complaint, Incident Report.'
+                acc_file = form_data['acc']
+                acc_filename = acc_file.filename
+                if acc_filename == "":
+                    return redirect('failure')
+                if isFileAllowed(acc_file):
+                    acc_filename = secure_filename(acc_filename)
+                    acc_file = Image.open(acc_file)
+                    doc, image_path = localUploadAndExtraction(acc_filename, acc_file)
+                    #FOR TESTING
+                    path = os.path.abspath("../SCDA/extract_text/extraction_tests/test_textdumps/Application for Criminal Complaint .txt")
+                    doc = open(path).read()
+                    acc_info = extract_application_for_criminal_complaint(doc)
+                    if acc_info["Name"] == '' or acc_info["Date of Birth"] == '' or acc_info["Social Security No."] == '':
+                        return "Application for criminal complaint could not be read. Please upload a clearer image."
+                    else:
+                        uuid = getUserID(acc_info["Name"], acc_info["Social Security No."][-4:], acc_info["Date of Birth"])
+                        formTable = models.forms(uuid, form_upload_date, form_upload_date, form_upload_date, form_upload_date)
+                        acc_insert = models.ACC(uuid, form_upload_date, image_path, acc_info["Summons"], acc_info["Hearing Requested"], acc_info["Court"], acc_info["Arrest Status of Accused"], acc_info["Arrest Date"], acc_info["In Custody"],acc_info["Officer ID No."],acc_info["Agency"],acc_info["Type"],acc_info["Name"],acc_info["Birth Surname"],acc_info["Address"],acc_info["Date of Birth"], acc_info["Place of Birth"], acc_info["Social Security No."], acc_info["PCF No."],acc_info["SID"],acc_info["Marital Status"],acc_info["Driver's License No."],acc_info["Driver's License State"],acc_info["Driver's License Exp. Year"],acc_info["Gender"],acc_info["Race"],acc_info["Height"],acc_info["Weight"],acc_info["Eyes"],acc_info["Hair"],acc_info["Ethnicity"],acc_info["Primary Language"],acc_info["Complexion"],acc_info["Scars/Marks/Tattoos"],acc_info["Employer Name"],acc_info["School Name"],acc_info["Day Phone"],acc_info["Mother Name"],acc_info["Mother Maiden Name"],acc_info["Father Name"],acc_info["Complainant Type"],acc_info["Police Dept."])
+                        db.session.add(formTable)
+                        db.session.commit()
+                        db.session.add(acc_insert)
+                        db.session.commit()
+                cc_file = form_data['cc']
+                cc_filename = cc_file.filename
+                if cc_filename == "":
+                    return redirect('failure')
+                if isFileAllowed(cc_file):
+                    cc_filename = secure_filename(cc_filename)
+                    cc_file = Image.open(cc_file)
+                    doc, image_path = localUploadAndExtraction(cc_filename, cc_file)
+                    cc_info = extract_criminal_complaint(doc)
+                    cc_insert = models.CC(uuid, form_upload_date, image_path, cc_info["docket"], cc_info["name"], cc_info["dob"], cc_info["doc"], 
+                            cc_info["doo"], cc_info["doa"], cc_info["ned"], cc_info["obtn"], cc_info["irn"], cc_info["court_address"], 
+                            cc_info["defendant_address"], cc_info["offense_codes"])
+                    db.session.add(cc_insert)
+                    db.session.commit()
+                ir_file = form_data['ir']
+                ir_filename = ir_file.filename
+                if ir_filename == "":
+                    return redirect('failure')
+                if isFileAllowed(ir_file):
+                    ir_filename = secure_filename(ir_filename)
+                    ir_file = Image.open(ir_file)
+                    doc, image_path = localUploadAndExtraction(ir_filename, ir_file)
+                    ir_info = extract_incident_report(doc)
+                    ir_insert = models.IR(uuid, form_upload_date, image_path, ir_info["Case Number"],ir_info["CAD Incident Number"],ir_info["Report Type"],ir_info["Date / Time Occurred"],ir_info["Date / Time Reported"],ir_info["Public Narrative"])
+                    db.session.add(ir_insert)
+                    db.session.commit()
+                else:
+                    return redirect('failure')
+                addOptional = addOptionalForms(form_data, uuid, formTable, form_upload_date)
+                print(addOptional)
+                if not addOptional:
+                    return redirect('failure')
+                return redirect('success')
+        elif request.method == 'GET':
+            print('GET request received.')
+            return render_template('admin.html')
+            #return 'Please send a post request with at least the following forms: Application for Criminal Complaint, Criminal Complaint, Incident Report.'
+    except:
+        return redirect('failure')
             
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
