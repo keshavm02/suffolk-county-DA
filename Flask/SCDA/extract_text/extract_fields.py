344050040b3006c1e481ec196a3278ab89ac793a
Gaby Nitorreda <gabriela.nitorreda@gmail.com>
2020-04-30 23:23:36 -0400

    removed path print statement; changed cari in probation record to string

--- Flask/SCDA/extract_text/extract_fields.py
+++ Flask/SCDA/extract_text/extract_fields.py
@@ -231,6 +231,7 @@ def extract_probation_form(raw_document):
     for i in range(len(fields['CARI'])):
         fields['CARI'][i] = fields['CARI'][i].replace('KT#', 'CA DKT#')
     fields['CARI'] = fields['CARI'][1:]
+    fields['CARI'] = str(fields['CARI'])
     return fields
 
 
