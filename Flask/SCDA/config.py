a1930cac4862d01a2906e763704c75d7a989a79c
Gaby Nitorreda <gabriela.nitorreda@gmail.com>
2020-04-30 22:41:24 -0400

    switched to sqlite

--- Flask/SCDA/config.py
+++ Flask/SCDA/config.py
@@ -20,3 +20,27 @@ app.config['UPLOAD_JSON'] = UPLOAD_JSON
 app.config['UPLOAD_FINAL'] = UPLOAD_FINAL
 
 app.secret_key = os.urandom(24)
+app.config['DEBUG'] = True
+#https://docs.sqlalchemy.org/en/13/core/engines.html
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///constituents"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER #configuration on startup
+app.config['UPLOAD_JSON'] = UPLOAD_JSON
+app.config['UPLOAD_FINAL'] = UPLOAD_FINAL
+
+app.secret_key = os.urandom(24)
+app.config['DEBUG'] = True
+#https://docs.sqlalchemy.org/en/13/core/engines.html
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///constituents"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER #configuration on startup
+app.config['UPLOAD_JSON'] = UPLOAD_JSON
+app.config['UPLOAD_FINAL'] = UPLOAD_FINAL
+
+app.secret_key = os.urandom(24)
+app.config['DEBUG'] = True
+#https://docs.sqlalchemy.org/en/13/core/engines.html
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///constituents"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER #configuration on startup
+app.config['UPLOAD_JSON'] = UPLOAD_JSON
+app.config['UPLOAD_FINAL'] = UPLOAD_FINAL
+
+app.secret_key = os.urandom(24)
